abd506fc54a03a596c2ad14f7efa6047ff1e0c39
MysticalFists <Mystical_Fists2000@yahoo.com>
2015-05-13 23:18:49 -0500

    Updating to 0.4

--- ui/main/shared/js/build.js
+++ ui/main/shared/js/build.js
@@ -17,7 +17,7 @@ function BuildHotkeyModel() {
             "/pa/hackedunits/orbital/deep_space_radar/deep_space_radar.json": ["utility", 12],
             "/pa/hackedunits/land/energy_storage/energy_storage.json": ["utility", 13],
             "/pa/hackedunits/land/metal_storage/metal_storage.json": ["utility", 14],
-                              
+		
             "/pa/hackedunits/sea/naval_factory_adv/naval_factory_adv.json": ["factory", 6],           
             "/pa/hackedunits/air/air_factory_adv/air_factory_adv.json": ["factory", 7],
             "/pa/hackedunits/land/bot_factory_adv/bot_factory_adv.json": ["factory", 8],
@@ -27,7 +27,8 @@ function BuildHotkeyModel() {
             "/pa/hackedunits/air/air_factory/air_factory.json": ["factory", 12],
             "/pa/hackedunits/land/bot_factory/bot_factory.json": ["factory", 13],
             "/pa/hackedunits/land/vehicle_factory/vehicle_factory.json": ["factory", 14],
-
+            "/pa/hackedunits/land/unit_cannon/unit_cannon.json": ["factory", 0],
+			
             "/pa/hackedunits/land/laser_defense_adv/laser_defense_adv.json": ["combat", 0],                   
             "/pa/hackedunits/land/tactical_missile_launcher/tactical_missile_launcher.json": ["combat", 2],
             "/pa/hackedunits/land/laser_defense/laser_defense.json": ["combat", 5],
@@ -51,7 +52,7 @@ function BuildHotkeyModel() {
             "/pa/hackedunits/land/tank_armor/tank_armor.json": ["vehicle", 12],
             "/pa/hackedunits/land/land_scout/land_scout.json": ["vehicle", 13],
             "/pa/hackedunits/land/aa_missile_vehicle/aa_missile_vehicle.json": ["vehicle", 14],    
-
+			
             "/pa/hackedunits/land/fabrication_bot_adv/fabrication_bot_adv.json": ["bot", 5],   
             "/pa/hackedunits/land/assault_bot_adv/assault_bot_adv.json": ["bot", 6],
             "/pa/hackedunits/land/bot_sniper/bot_sniper.json": ["bot", 7],
@@ -62,8 +63,7 @@ function BuildHotkeyModel() {
             "/pa/hackedunits/land/bot_grenadier/bot_grenadier.json": ["bot", 12],
             "/pa/hackedunits/land/fabrication_bot_combat/fabrication_bot_combat.json": ["bot", 13],
             "/pa/hackedunits/land/bot_tactical_missile/bot_tactical_missile.json": ["bot", 9],
-     
-            
+			
             "/pa/hackedunits/orbital/orbital_laser/orbital_laser.json": ["orbital", 6],
             "/pa/hackedunits/orbital/radar_satellite_adv/radar_satellite_adv.json": ["orbital", 7],
             "/pa/hackedunits/orbital/solar_array/solar_array.json": ["orbital", 5],
@@ -76,21 +76,24 @@ function BuildHotkeyModel() {
             "/pa/hackedunits/orbital/mining_platform/mining_platform.json": ["orbital", 8],
                  
             "/pa/hackedunits/air/fabrication_aircraft_adv/fabrication_aircraft_adv.json": ["air", 5],
-            "/pa/hackedunits/air/gunship/gunship.json": ["air", 6],
-            "/pa/hackedunits/air/bomber_adv/bomber_adv.json": ["air", 7],
-            "/pa/hackedunits/air/transport/transport.json": ["air", 8],
+			"/pa/hackedunits/air/fighter_adv/fighter_adv.json": ["air", 6],
+            "/pa/hackedunits/air/gunship/gunship.json": ["air", 7],
+            "/pa/hackedunits/air/bomber_adv/bomber_adv.json": ["air", 8],
             "/pa/hackedunits/air/fabrication_aircraft/fabrication_aircraft.json": ["air", 10],
             "/pa/hackedunits/air/fighter/fighter.json": ["air", 11],
             "/pa/hackedunits/air/bomber/bomber.json": ["air", 12],
             "/pa/hackedunits/air/air_scout/air_scout.json": ["air", 13],
-
+            "/pa/hackedunits/air/transport/transport.json": ["air", 14],
+			
             "/pa/hackedunits/sea/fabrication_ship_adv/fabrication_ship_adv.json": ["sea",5],
             "/pa/hackedunits/sea/battleship/battleship.json": ["sea", 6],
             "/pa/hackedunits/sea/missile_ship/missile_ship.json": ["sea", 7],
+            "/pa/hackedunits/sea/nuclear_sub/nuclear_sub.json": ["sea", 8],
             "/pa/hackedunits/sea/fabrication_ship/fabrication_ship.json": ["sea", 10],
             "/pa/hackedunits/sea/frigate/frigate.json": ["sea", 11],
             "/pa/hackedunits/sea/destroyer/destroyer.json": ["sea", 12],
-            "/pa/hackedunits/sea/sea_scout/sea_scout.json": ["sea", 13],
+            "/pa/hackedunits/sea/attack_sub/attack_sub.json": ["sea", 13],
+            "/pa/hackedunits/sea/sea_scout/sea_scout.json": ["sea", 14],
 
             "/pa/hackedunits/land/land_mine/land_mine.json": ["ammo", 10],
             "/pa/hackedunits/land/anti_nuke_launcher/anti_nuke_launcher_ammo.json": ["ammo", 13],
